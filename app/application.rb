c2e9991c788f0d1c0bb3f75b0211e6089f97d01e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 02:20:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    time = 
+    time = Time.authenticate_or_request_with_http_digest
     resp.write "Hello, World"
     resp.finish
   end
